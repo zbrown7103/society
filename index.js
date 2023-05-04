f1702a535e902a60ecc2138dbb811bc3e413ee61
zbrown7103 <zbrown7103@gmail.com>
2023-05-04 10:20:46 -0600

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+function getRandomItems(items) {
+    const keys = Object.keys(items);
+    const randomItems = {};
+
+    for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * keys.length);
+        const key = keys[randomIndex];
+        randomItems[key] = items[key];
+        keys.splice(randomIndex, 1);
+    }
+
+    return randomItems;
+}
+
+async function displayItems(inputAmount) {
+    const response = await fetch("items.json");
+    const items = await response.json();
+    const randomItems = getRandomItems(items);
+
+    const list = document.getElementById("result_list");
+    list.innerHTML = "";
+
+    for (const [key, value] of Object.entries(randomItems)) {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${(inputAmount / value).toFixed(2)} ${key}`;
+        list.appendChild(listItem);
+    }
+}
+
 document.getElementById("go_button").addEventListener("click", async () => {
     const inputAmountValue = document.getElementById("input_amount").value;
     const cleanInputAmountValue = inputAmountValue.replace('$', '');
@@ -8,15 +37,5 @@ document.getElementById("go_button").addEventListener("click", async () => {
         return;
     }
 
-    const response = await fetch("items.json");
-    const items = await response.json();
-
-    const list = document.getElementById("result_list");
-    list.innerHTML = "";
-
-    for (const [key, value] of Object.entries(items)) {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${(inputAmount / value).toFixed(2)} ${key}`;
-        list.appendChild(listItem);
-    }
+    await displayItems(inputAmount);
 });
